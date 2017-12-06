736b18078e6f7494b7a3fbddde86557d6bcc3941
Henrique Figueiredo de Souza <riquefsouza@gmail.com>
2017-12-06 11:51:26 -0200

    HFSGuardaDiretorio em Object Pascal programado no Lazarus

---
+++ HFSGuardaDiretorio_Lazarus_Classes/uSobre.pas
@@ -0,0 +1,37 @@
+unit uSobre;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Controls, StdCtrls, Classes, Forms, ExtCtrls, SysUtils,
+  Graphics;
+
+type
+
+  { TFrmSobre }
+
+  TFrmSobre = class(TForm)
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    btnOk: TButton;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  FrmSobre: TFrmSobre;
+
+implementation
+
+{$R *.lfm}
+
+{ TFrmSobre }
+
+
+end.
